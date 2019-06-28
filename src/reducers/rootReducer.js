38c7fe943ea527b7324da666a1dc1a03dc4c6776
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-28 12:37:35 -0400

    added reducers

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import loadingReducer from './loadingReducer';
+// import userReducer from './userReducer';
+
+const rootReducer = () => combineReducers({
+  loading: loadingReducer
+  // ,
+    // user: userReducer
+});
+
+export default rootReducer;
