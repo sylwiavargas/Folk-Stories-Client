d442ebb05cc9f1d7976c503f0588d66a5f4c6305
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-07-03 00:07:47 -0400

    added goo

--- src/components/notfound.js
+++ src/components/notfound.js
@@ -1,3 +1,65 @@
-import React from 'react'
-const Notfound = () => <h1>Not found</h1>
-export default Notfound
+import React, { Component, Fragment } from 'react';
+import Goo from './Goo'
+
+class Notfound extends Component {
+
+  state = {
+    time: 20
+  };
+
+  startTimer() {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time
+    })
+    this.timer = setInterval(() => this.setState({
+      time: this.state.time - 1
+    }), 1000);
+  }
+
+  componentDidMount() {
+    this.startTimer()
+  }
+
+  goBack = () => {
+    this.props.history.push('/')
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.time < 20 ?
+          <Fragment>
+          <Goo />
+          <p className="main">
+          <div>
+          <h1>Oof, non-existent page!</h1>
+          <h2> You will be redirected in {this.state.time} seconds. </h2>
+          <button onClick={this.goBack}>Go back now</button>
+          <h2> Or play with goo. </h2>
+          </div>
+          </p>
+          </Fragment>
+          :
+          this.state.time !== 0 ?
+          <Fragment>
+          <p className="main">
+          <div>
+          <h1>Oof, non-existent page!</h1>
+          <h2> You will be redirected in {this.state.time} seconds. </h2>
+          <button onClick={this.goBack}>Go back now</button>
+          </div>
+          </p>
+          </Fragment>
+          :
+          this.goBack()
+        }
+      </div>
+    )
+  }
+
+}
+
+export default Notfound;
